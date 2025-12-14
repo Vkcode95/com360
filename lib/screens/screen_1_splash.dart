cf397a37465603309c9c7fa83035be6472b30863
Vkcode95 <139173564+Vkcode95@users.noreply.github.com>
2025-12-14 17:22:52 +0530

    Phase 1: Enable safe splash navigation

--- lib/screens/screen_1_splash.dart
+++ lib/screens/screen_1_splash.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
 
-class Screen1Splash extends StatelessWidget {
+class Screen1Splash extends StatefulWidget {
   const Screen1Splash({super.key});
+
+  @override
+  State<Screen1Splash> createState() => _Screen1SplashState();
+}
+
+class _Screen1SplashState extends State<Screen1Splash> {
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Future.delayed(const Duration(seconds: 2), () {
+        if (mounted) {
+          Navigator.pushReplacementNamed(context, '/screen2');
+        }
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
